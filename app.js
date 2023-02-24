260a976572e48b0a00798de461192fa1bd22ef96
resulgzl98 <rslgzl1998@gmail.com>
2023-02-24 22:40:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.ani1',{delay:250});
+ScrollReveal().reveal('.ani2',{delay:450});
+ScrollReveal().reveal('.ani3',{delay:650});
+ScrollReveal().reveal('.ani4',{delay:850});
+ScrollReveal().reveal('.ani5',{delay:1050});
+ScrollReveal().reveal('.ani6',{delay:1250});
+
+window.sr=ScrollReveal();
+sr.reveal('.text',{
+origin: 'top',
+duration : 1000,
+distance: '25rem',
+});
